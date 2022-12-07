e58b77fe49fac4f4b81c3eb92d47b786805f0571
Alexperovo <alexperovo@gmail.com>
2022-12-07 23:00:07 +0300

    работает

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+int[,] matrix = new int[3, 4];
+FillArray(matrix);
+PrintArray(matrix);
+SortArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+
+void SortArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            for (int f = 0; f < matr.GetLength(1) - 1; f++)
+            {
+                if (matr[i, f] < matr[i, f + 1])
+
+                {
+                    int temp = matr[i, f + 1];
+                    matr[i, f + 1] = matr[i, f];
+                    matr[i, f] = temp;
+                }
+            }
+        }
+    }
+}
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
